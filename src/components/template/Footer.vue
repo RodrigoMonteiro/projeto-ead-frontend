31a3117e2de598c8d20dfec773a366589da3dcf1
Rodrigo <rodrigomonteiiro17@gmail.com>
2020-10-15 11:19:30 -0300

    Estrutura inicial do frontend

---
+++ src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+      <span> <strong>Trabalho desenvolvido para a disciplina de EaD</strong></span>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Footer'
+}
+</script>
+
+<style>
+.footer{
+    grid-area: footer;
+    background-color: #DDD;
+    color:  #333;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 10px ;
+}
+</style>
